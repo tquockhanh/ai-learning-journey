fe27e18d20c0168e03bc76b5ec161d5a6eac2e65
tquockhanh <tqkhanh244@gmail.com>
2026-03-10 21:12:55 +0700

    Day 2: start python basics

---
+++ python/day2_basics.py
@@ -0,0 +1,26 @@
+"""
+Day 2
+Python basics for AI learning journey
+"""
+
+# variables
+
+a = 10
+b = 20
+
+print(a + b)
+
+
+# list
+
+nums = [1, 2, 3, 4, 5]
+
+print(nums)
+print(nums[0])
+print(nums[-1])
+
+
+# loop
+
+for x in nums:
+    print(x)
